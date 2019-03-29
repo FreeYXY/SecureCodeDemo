6ed5c6f6f86df933cd06b1646a3b49326e991be2
YXY <yangxiaoyang@techwis.cn>
2019-03-29 15:23:57 +0800

    完善demo

--- SecureCodeDemo/SecureCodeDemo/AppDelegate.m
+++ SecureCodeDemo/SecureCodeDemo/AppDelegate.m
@@ -7,7 +7,6 @@
 //
 
 #import "AppDelegate.h"
-#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -18,13 +17,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    
-    ViewController *vc = [[ViewController alloc]init];
-    UINavigationController *nav = [[UINavigationController alloc]initWithRootViewController:vc];
-
-    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
-    self.window.rootViewController = nav;
-    [self.window makeKeyAndVisible];
     return YES;
 }
 
