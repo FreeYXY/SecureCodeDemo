aa1c7b2331073a472a0eb47b057aa2fbb5a20ee2
YXY <yangxiaoyang@techwis.cn>
2019-03-29 11:54:19 +0800

    创建项目模板

---
+++ SecureCodeDemo/SecureCodeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SecureCodeDemo
+//
+//  Created by YXY on 2019/3/29.
+//  Copyright © 2019 Techwis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
